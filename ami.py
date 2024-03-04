6869815cbc578d93076e702f9f115ab7a02f9e5f
Madhubabu11 <111720198+Madhubabu11@users.noreply.github.com>
2024-03-04 16:45:59 +0530

    Update ami.py

--- ami.py
+++ ami.py
@@ -7,7 +7,7 @@ def delete_amis(access_key_id, secret_access_key, region):
     client = boto3.client(
         'ec2',
         aws_access_key_id=access_key_id,
-        aws_secret_access_key=secret_access_key
+        aws_secret_access_key=secret_access_key,
         region_name=region
     )
 # Describe instances
